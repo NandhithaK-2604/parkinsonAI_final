e509f5591e913f8afaa47778d3bb794a653708e8
Nandhitha K <155251925+NandhithaK-2604@users.noreply.github.com>
2025-11-26 17:16:33 +0530

    Update SpectrogramsPage.tsx

--- src/pages/SpectrogramsPage.tsx
+++ src/pages/SpectrogramsPage.tsx
@@ -1,6 +1,6 @@
 
 
-import React, { useState } from "react";
+import  { useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 // Import local image assets for graphs
